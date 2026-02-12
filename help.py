fea152d9cc27acaaf0eb3eb68b3dc0dc14f6548c
Luke <lukeenness@outlook.com>
2026-02-12 13:29:24 +0000

    first commit

---
+++ help.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import sys
+
+def plotData(filename):
+    with open(filename, "r") as file:
+        lines = file.readlines()
+        x = []
+        y = []
+        for line in lines[38:]:
+            data = line.split()
+            if len(data) == 2:
+                x.append(float(data[0]))
+                y.append(float(data[1]))
+    plt.plot(x, y)
+
+
+    plt.xlabel('X-axis')
+    plt.ylabel('Y-axis')
+    plt.title('Plot of Data from File')
+
+    plt.grid()
+    plt.show()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python help.py <filename>")
+        sys.exit(1)
+    filename = sys.argv[1] + ".txt"
+    plotData(filename)
+
+
+
+# def validateFile(filename):
+
+# def validateFileName(filename):
+
+
+
+
