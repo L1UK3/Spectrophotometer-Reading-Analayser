a626dbf02fa43c843fb8964d9cc4f72a8a3fa330
Luke <lukeenness@outlook.com>
2026-02-12 18:27:52 +0000

    Script properly reads data in the correct order and appends only the data to the list

--- main.py
+++ main.py
@@ -2,33 +2,34 @@ import matplotlib.pyplot as plt
 import sys
 
 def plotData(filename):
+
+    xLabel = "X-axis"
+    yLabel = "Y-axis"
+
     with open(filename, "r") as file:
         lines = file.readlines()
         x = []
         y = []
-        for line in lines[38:]:
+
+        for line in lines[::-1]:
             data = line.split()
+
             if len(data) == 2:
-                x.append(float(data[0]))
-                y.append(float(data[1]))
+                try:
+                    x.append(float(data[0]))
+                    y.append(float(data[1]))
+                except ValueError:
+                    continue
+
+
     plt.plot(x, y)
 
-
-    plt.xlabel('X-axis')
-    plt.ylabel('Y-axis')
-    plt.title('Plot of Data from File')
+    plt.xlabel(xLabel)
+    plt.ylabel(yLabel)
+    plt.title(filename)
 
     plt.grid()
     plt.show()
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python help.py <filename>")
-        sys.exit(1)
-    filename = sys.argv[1] + ".txt"
-    plotData(filename)
-
 
 
 # def validateFile(filename):
@@ -36,5 +37,17 @@ if __name__ == "__main__":
 # def validateFileName(filename):
 
 
+"""
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python help.py <filename>")
+        sys.exit(1)
+    filename = sys.argv[1] + ".txt"
+    plotData(filename)
+"""
 
+
+
+
+plotData("blank_02-02.txt")
 
