cfa0774550924d9ab5fa1c63cad952b5c44ff8a8
Luke Enness <lukeenness@outlook.com>
2026-02-19 15:40:03 +0000

    Documented python script and added a __main__ module

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
+
+"""Spectrometer data plotting script
+
+Initial python script to read data from the file and plot it using matplotlib.
+
+This script assumes 'filename' is a text file where each line contains two numbers (x and y values) separated by whitespace.
+The script reads the data, stores it in two lists, and then plots it using matplotlib.
+
+This script requires the matplotlib library to be installed. You can install it using pip if you don't have it already:
+pip install matplotlib
+
+This file can be imported as a module and contains the following functions:
+- retrieveData: Reads data from a file and returns two lists of x and y values.
+"""
+
+
 import matplotlib.pyplot as plt
 
-def retrieveData(filename):
+def retrieveData(filename: str) -> tuple[list[float], list[float]]:
+    """
+    Opens a file, reads its contents, and extracts x and y values from it. 
+    The function expects each line of the file to contain two numbers (x and y) separated by whitespace. 
+    
+    Args:
+        filename (str): The name of the file to read data from in the active directory.
+
+    Returns:
+        tuple[list[float], list[float]]: A tuple containing two lists: the first list contains x values and the second list contains y values.
+    
+    """
 
     with open(filename, "r") as file:
         lines = file.readlines()
@@ -11,22 +38,22 @@ def retrieveData(filename):
             data = line.split()
 
             if len(data) == 2:
-                print(data)
+                print(data) # Just for testing
                 try:
                     x.append(float(data[0]))
                     y.append(float(data[1]))
                 except ValueError:
                     return (x,y)
+        
+        return (x, y)
                 
 
+if __name__ == "__main__":
+    x,y = retrieveData(input())#"blank_02-02.TXT")
+    plt.plot(x,y)
+    plt.grid()
+    plt.show()
 
-
-x,y = retrieveData(input())#"blank_02-02.TXT")
-
-plt.plot(x,y)
-
-plt.grid()
-plt.show()
 
 
 
