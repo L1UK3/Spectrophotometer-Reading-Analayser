51e5c5867d3f7379cab3841df55640e08a860356
Luke Enness <lukeenness@outlook.com>
2026-02-19 15:58:00 +0000

    Adjudted multi-line docstring spacing

--- main.py
+++ main.py
@@ -3,10 +3,12 @@
 
 Initial python script to read data from the file and plot it using matplotlib.
 
-This script assumes 'filename' is a text file where each line contains two numbers (x and y values) separated by whitespace.
+This script assumes 'filename' is a text file where each line contains two numbers 
+(x and y values) separated by whitespace.
 The script reads the data, stores it in two lists, and then plots it using matplotlib.
 
-This script requires the matplotlib library to be installed. You can install it using pip if you don't have it already:
+This script requires the matplotlib library to be installed. 
+You can install it using pip if you don't have it already:
 pip install matplotlib
 
 This file can be imported as a module and contains the following functions:
@@ -19,13 +21,18 @@ import matplotlib.pyplot as plt
 def retrieveData(filename: str) -> tuple[list[float], list[float]]:
     """
     Opens a file, reads its contents, and extracts x and y values from it. 
-    The function expects each line of the file to contain two numbers (x and y) separated by whitespace. 
+    The function expects each line of the file to contain two numbers 
+    (x and y) separated by whitespace. 
     
     Args:
-        filename (str): The name of the file to read data from in the active directory.
+        filename (str): 
+            The name of the file to read data from in the active directory.
 
     Returns:
-        tuple[list[float], list[float]]: A tuple containing two lists: the first list contains x values and the second list contains y values.
+        tuple[list[float], list[float]]: 
+            A tuple containing two lists: 
+            the first list contains x values
+            the second list contains y values.
     
     """
 
@@ -43,14 +50,14 @@ def retrieveData(filename: str) -> tuple[list[float], list[float]]:
                     x.append(float(data[0]))
                     y.append(float(data[1]))
                 except ValueError:
-                    return (x,y)
+                    return (x, y)
         
         return (x, y)
                 
 
 if __name__ == "__main__":
-    x,y = retrieveData(input())#"blank_02-02.TXT")
-    plt.plot(x,y)
+    coorinates = retrieveData("test.TXT")
+    plt.plot(coorinates[0], coorinates[1])
     plt.grid()
     plt.show()
 
